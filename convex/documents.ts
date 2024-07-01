760225279ed1fad02b857c204090f8b790d15680
Jatin Banga <bangajatin4@gmail.com>
2024-07-01 05:37:46 +0530

    added icon and cover image with backend using edge store

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,7 +1,6 @@
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
-import { title } from "process";
 
 export const archive = mutation({
     args: { id: v.id("documents") },
@@ -319,5 +318,34 @@ export const removeIcon = mutation({
         });
 
         return document;
+    }
+});
+
+export const removeCoverImage = mutation({
+    args: { id : v.id("documents")},
+    handler: async (ctx,args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if(!identity){
+            throw new Error("Unauthenticated");
+        }
+
+        const userId = identity.subject;
+        const existingDocument = await ctx.db.get(args.id);
+
+        if(!existingDocument) {
+            throw new Error("Not found");
+        }
+
+        if(existingDocument.userId!==userId){
+            throw new Error("Unauthorized");
+        }
+
+        const document = await ctx.db.patch(args.id, {
+            coverImage: undefined,
+        });
+
+        return document;
+
     }
 });
